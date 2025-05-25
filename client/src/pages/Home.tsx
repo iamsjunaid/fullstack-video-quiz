6a68fd40f103493bc57529bacceaf166c7e95966
Junaid Syed <sjunaid626@gmail.com>
2025-05-25 18:49:52 +0530

    fix: adjust max width of upload container in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import UploadForm from "../components/UploadForm";
 
 const Home: React.FC = () => {
     return (
-        <div className="p-10 max-w-xl mx-auto">
+        <div className="p-10 max-w-4xl mx-auto">
             <h1 className="text-2xl font-bold mb-4">Upload Lecture Video</h1>
             <UploadForm />
         </div>
